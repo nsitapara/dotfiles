ac9743a8ee76d7e87d774e7a11afbbfd929141e3
Nish Sitapara <nish@herasight.com>
2025-12-08 11:36:33 -0800

    Update neovim.lua

--- themes/.config/omarchy/themes/catppuccin-mocha/neovim.lua
+++ themes/.config/omarchy/themes/catppuccin-mocha/neovim.lua
@@ -7,12 +7,9 @@ return {
 	},
 	{
 		"catppuccin/nvim",
-		opts = function(_, opts)
-			local module = require("catppuccin.groups.integrations.bufferline")
-			if module then
-				module.get = module.get_theme
-			end
-			return opts
-		end,
+		name = "catppuccin",
+		opts = {
+			flavour = "mocha",
+		},
 	},
 }
